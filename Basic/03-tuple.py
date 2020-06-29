ea11a46bf066cf551dca8ec6b91bfdce8e8d77cd
liao2000 <liao2000@livemail.tw>
2020-06-30 02:49:23 +0800

    tuple

---
+++ Basic/03-tuple.py
@@ -0,0 +1,11 @@
+# tuple is ordered and unchangeable
+thistuple = (0,1,2,3,4)
+print(thistuple[1])
+
+# we cannot change tuple's value once it is created
+# foreach
+for i in thistuple:
+    print(i)
+
+# construct tuple by using constructor
+tupleA = tuple(("yunataco","sueka","maikichi","yupon","kana"))
