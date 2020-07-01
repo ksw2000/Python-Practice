846cae949100f2dbba989a27bcd33b0d0caef4fc
liao2000 <liao2000@livemail.tw>
2020-07-01 11:52:47 +0800

    module

---
+++ Module/Dict.py
@@ -0,0 +1,11 @@
+thisDict = {
+    "brand": "hadname",
+    "url":   "https://had.name",
+    "year":  2015
+}
+
+anotherDict = {
+    "yuna":     "2004-11-07",
+    "maikichi": "2005-01-30",
+    "sueka":    "2003-01-30"
+}
---
+++ Module/Use_Modules.py
@@ -0,0 +1,10 @@
+import Hello
+Hello.hello("Python")
+
+#use `as` give an alias(別名)
+import Dict as x
+print(x.thisDict)
+
+#import a part of file
+from Dict import anotherDict
+print(anotherDict)
